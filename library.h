981bc4571e7be07dd09e45ffeccc0853dcb90dbd
goulart <leonardo@roffor.com.br>
2017-03-10 00:33:42 -0300

    comentarios na biblioteca

--- library.h
+++ library.h
@@ -4,12 +4,12 @@
 #include <iostream>
 
 void numero(int [], const std::string& );
-
+//contador de numeros
 void letraM(int [], const std::string& );
-
+//contador de letras maiusculas
 void letram(int [], const std::string& );
-
+//contador de letras minusculas
 void imprimir(int [], int [],int []);
-
+//impressao no terminal
 
 #endif
